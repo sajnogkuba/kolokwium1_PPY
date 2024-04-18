bbc049e39ed16a8c1d22880fcabcf76b984f1c3b
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-19 00:34:18 +0200

    ex 3 C01

--- Kolokwium1_PPY/C01_Control_Flow/PPY2_3.py
+++ Kolokwium1_PPY/C01_Control_Flow/PPY2_3.py
@@ -1,8 +1,20 @@
-# 3A.
-# napisz program, który policzy ile jest cyfr w liczbie, przy pomocy funckji len()
+def countDigitsStr(number):
+    return len(str(number))
 
+
+def countDigits(number):
+    digits = 0
+    while number > 0:
+        digits += 1
+        number = number // 10
+    return digits
+
+
+# 3A.
+# Napisz program, który policzy ile jest cyfr w liczbie, przy pomocy funckji len()
+print(countDigitsStr(123456))
 
 # 3B.
-# napisz program, który policzy ile jest cyfr w tej samej liczbie innym sposobem
+# Napisz program, który policzy ile jest cyfr w tej samej liczbie innym sposobem
 # pomyśl o szybkości obliczeń
-
+print(countDigits(123456))
