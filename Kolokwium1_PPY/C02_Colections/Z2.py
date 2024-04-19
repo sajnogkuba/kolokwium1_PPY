d1d700a6408497cb900a1023278e40efae0bf8a4
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-19 13:08:02 +0200

    add ex

---
+++ Kolokwium1_PPY/C02_Colections/Z2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+
+Napisz definicje, która przeprowadzi analizę tekstu. Program powinien wykonać następujące czynności:
+- Wczytać tekst
+- Podzielić tekst na słowa.
+- Usunąć znaki interpunkcyjne i zamienić wszystkie litery na małe litery.
+- Utworzyć kolekcję słów i zliczyć ile razy każde słowo występuje w tekście.
+- Wyświetlić 10 najczęściej występujących słów wraz z ich liczbami wystąpień.
+- Wyświetlić 10 najrzadziej występujących słów wraz z ich liczbami wystąpień.
+- Wyświetlić liczbę unikalnych słów w tekście.
+- Wyświetlić średnią długość słowa w tekście.
+'''
