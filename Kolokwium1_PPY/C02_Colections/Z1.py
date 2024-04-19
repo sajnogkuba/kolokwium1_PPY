e4efa3981ec70002d7d2b9c6f587907766a7f8d5
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-19 13:07:25 +0200

    ex 1 C02

--- Kolokwium1_PPY/C02_Colections/Z1.py
+++ Kolokwium1_PPY/C02_Colections/Z1.py
@@ -1,9 +1,29 @@
 # -*- coding: utf-8 -*-
 
-'''
-Napisz funkcję slowa_podobne(s, words), która wyświetli słowa, które są ,,podobne" do napisu s
+"""
+Napisz funkcję słowa_podobne (s, words), która wyświetli słowa, które są ""podobne" do napisu s
 w tym sensie, że składające się z tych samych znaków — ale ewentualnie występujących inną liczbę razy
 #PRZYKŁAD
-slowa_podobne('one', ['one', 'two', 'none', 'three', 'neon', 'eon']) 
->> none neon eon
-'''
+słowa_podobne ('one', ['one', 'two', 'none', 'three', 'neon', 'eon'])
+» none neon eon
+"""
+
+
+def getLettersSet(word):
+    set_of_letters = set()
+    for letter in word:
+        set_of_letters.add(letter)
+    return set_of_letters
+
+
+def similarWords(s, words):
+    result = list()
+    base_set = getLettersSet(s)
+    for word in words:
+        if base_set == getLettersSet(word) and s != word:
+            result.append(word)
+    for word in result:
+        print(word, end=' ')
+
+
+similarWords('one', ['one', 'two', 'none', 'three', 'neon', 'eon'])
