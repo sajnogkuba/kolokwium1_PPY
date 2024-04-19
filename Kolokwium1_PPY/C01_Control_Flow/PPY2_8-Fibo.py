ad9ce25e1c04306f48c1d6f52cae3d3c03d489f1
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-19 12:37:34 +0200

    ex 8 C01

--- Kolokwium1_PPY/C01_Control_Flow/PPY2_8-Fibo.py
+++ Kolokwium1_PPY/C01_Control_Flow/PPY2_8-Fibo.py
@@ -1,4 +1,11 @@
 # napisz kod, który wyświetli ciąg fibonnaciego do 10 miejsc (https://pl.wikipedia.org/wiki/Ci%C4%85g_Fibonacciego)
 # pierwsze dwie liczby w ciągu
-num1, num2 = 0, 1
+def fiboFunction(amount_of_fibonacci_numbers):
+    fibonacci_numbers = [0, 1]
+    for i in range(2, amount_of_fibonacci_numbers):
+        fibonacci_numbers.append((fibonacci_numbers[i - 2] + fibonacci_numbers[i - 1]))
+    return fibonacci_numbers
+
+
 print("Ciąg Fibonacciego:")
+print(fiboFunction(50))
