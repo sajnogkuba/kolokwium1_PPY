62861932fe4d96ef9814877b305120e2833c82bd
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-20 10:42:17 +0200

    ex 3 C02 cz.1

--- Kolokwium1_PPY/C02_Colections/Z3.py
+++ Kolokwium1_PPY/C02_Colections/Z3.py
@@ -1,12 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Napisz program, który umożliwia zarządzanie książką adresową. Program powinien umożliwiać użytkownikowi wykonywanie następujących operacji:
+Napisz program, który umożliwia zarządzanie książką adresową. Program powinien umożliwiać użytkownikowi
+wykonywanie następujących operacji:
 
 Dodawanie nowego kontaktu do książki adresowej.
 Wyświetlanie wszystkich kontaktów z książki adresowej.
 Wyszukiwanie kontaktu po nazwie.
 Usuwanie kontaktu z książki adresowej.
 
-Użyj słownika, gdzie kluczem będzie nazwa kontaktu, a wartością będzie lista informacji o kontakcie (np. imię, nazwisko, numer telefonu).
+Użyj słownika, gdzie kluczem będzie nazwa kontaktu, a wartością będzie lista informacji o kontakcie (np. imię,
+nazwisko, numer telefonu).
 """
 
+
+def userInputChoice(options_list):
+    correct_input = False
+    user_input = 0
+    while not correct_input:
+        try:
+            user_input = int(input())
+        except ValueError:
+            user_input = 0
+        if user_input in options_list:
+            correct_input = True
+        else:
+            print('Invalid choose, try again')
+    return user_input
+
+
+phone_book = dict()
+print('What would you like to do?:')
+print('\t 1. Add a phone number')
+print('\t 2. See all phone numbers')
+print('\t 3. Search a phone number')
+print('\t 4. Delete a phone number')
