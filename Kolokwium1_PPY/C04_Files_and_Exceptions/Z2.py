76d7e25dbeb1fe2e73c5dd39d45f061ae8200c5b
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-21 21:53:56 +0200

    C04 ex 2

--- Kolokwium1_PPY/C04_Files_and_Exceptions/Z2.py
+++ Kolokwium1_PPY/C04_Files_and_Exceptions/Z2.py
@@ -10,6 +10,14 @@ Program ma obsługiwać błędy poprzez try i except i kończyć program przy:
 """
 
 
+def celsius2fahrenheit(celsius):
+    return (celsius * 9 / 5) + 32
+
+
+def fahrenheit2celsius(fahrenheit):
+    return ((fahrenheit - 32) * 5) / 9
+
+
 def userChoiceCF() -> str:
     user_choice = input("What would you like to convert?\n\t C - Celsius to Fahrenheit\n\t F - Fahrenheit to Celsius\n")
     if user_choice not in ["C", "F"]:
@@ -21,9 +29,24 @@ def userChoiceCF() -> str:
 is_running = True
 while is_running:
     try:
-        choice = userChoiceCF()
-    except ValueError:
-        print("Invalid choice, you can only choose C or F")
-        is_running = False
-        break
-
+        try:
+            choice = userChoiceCF()
+        except ValueError:
+            print("Invalid choice, you can only choose C or F")
+            is_running = False
+            break
+        try:
+            degrees = float(input("Type in value to convert: \n"))
+        except ValueError:
+            print("Invalid degree value.")
+            is_running = False
+            break
+        match choice:
+            case "C":
+                converted_degrees = celsius2fahrenheit(degrees)
+                print(f"{degrees}°C is {converted_degrees}°F")
+            case "F":
+                converted_degrees = fahrenheit2celsius(degrees)
+                print(f"{degrees}°F is {converted_degrees}°C")
+    except ZeroDivisionError:
+        print("You can't divide by zero")
