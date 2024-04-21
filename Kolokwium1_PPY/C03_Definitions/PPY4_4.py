15eae4e78b80f2f3bc73137f34088ca8773ec630
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-21 21:02:20 +0200

    C03 ex 4

--- Kolokwium1_PPY/C03_Definitions/PPY4_4.py
+++ Kolokwium1_PPY/C03_Definitions/PPY4_4.py
@@ -1,10 +1,31 @@
 # -*- coding: utf-8 -*-
-#Napisz funckje, która przyjmuje dowolną ilość liczb (args*) i znaków działania (kwargs**), gdzie znakami działania mogą być:
-#'+', '-', '*', '/'
-#Funckcja ma zwracać wynik działania na liczbach
-def kalkulator(*args, **kwargs):
-  #TWÓJ KOD
-  pass
+"""
+Napisz funkcję, która przyjmuje dowolną ilość liczb (args*) i znaków działania (kwargs**), gdzie znakami działania mogą
+być: '+', '-', '*', '/'
+Funkcja ma zwracać wynik działania na liczbach
+"""
+from unittest import case
 
-#Przykładowe użycie funkcji
-kalkulator(10,8,2, działanie_1='*', działanie_2="+") #82
+
+def kalkulator(*args, **kwargs):
+    result = 0
+    match kwargs["działanie_1"]:
+        case "+": result = args[0] + args[1]
+        case "-": result = args[0] - args[1]
+        case "*": result = args[0] * args[1]
+        case "/": result = args[0] / args[1]
+    for i in range(1, len(kwargs)):
+        match kwargs[f"działanie_{i + 1}"]:
+            case "+":
+                result += args[i + 1]
+            case "-":
+                result -= args[i + 1]
+            case "*":
+                result *= args[i + 1]
+            case "/":
+                result /= args[i + 1]
+    return result
+
+
+# Przykładowe użycie funkcji
+print(kalkulator(10, 8, 2, 2, działanie_1='*', działanie_2="+", działanie_3="/"))  # 82
