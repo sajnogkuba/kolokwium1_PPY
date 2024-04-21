4f03d1af5a8ac794953de76d3f4eedbe6f84dc58
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-21 13:41:15 +0200

    C03 ex 1 and 2

--- Kolokwium1_PPY/C03_Definitions/PPY4_1.py
+++ Kolokwium1_PPY/C03_Definitions/PPY4_1.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
-#Napisz funkcje silnii używając rekurencji
+# Napisz funkcje silnii używając rekurencji
 
+def factorialRecursive(number: int) -> int:
+    if number == 1:
+        return number
+    else:
+        return number * factorialRecursive(number - 1)
+
+
+print(factorialRecursive(6))
--- Kolokwium1_PPY/C03_Definitions/PPY4_2.py
+++ Kolokwium1_PPY/C03_Definitions/PPY4_2.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
-#Napisz funkcje validate_pesel, która przyjmuje ciąg znaków (str) i sprawdza, czy jest on poprawnie sformatowanym numerem PESEL.
-#Numer PESEL składa się z 11 cyfr. W celu sprawdzenia sumy kontrolnej użyj wag [1, 3, 7, 9, 1, 3, 7, 9, 1, 3].
-#Tutaj suma kontrolna to wynik odejmowania jedności od 10.
-#Funkcja powinna zwracać wartość logiczną: True jeśli numer PESEL jest poprawny, False w przeciwnym przypadku.
+"""
+Napisz funkcje validate_pesel, która przyjmuje ciąg znaków (str) i sprawdza, czy jest on poprawnie sformatowanym
+numerem PESEL. Numer PESEL składa się z 11 cyfr. W celu sprawdzenia sumy kontrolnej użyj wag [1, 3, 7, 9, 1, 3, 7, 9,
+1, 3]. Tutaj suma kontrolna to wynik odejmowania jedności od 10. Funkcja powinna zwracać wartość logiczną: True, jeśli
+numer PESEL jest poprawny, False w przeciwnym przypadku.
+"""
+
 
 def validate_pesel(pesel):
-  #TWÓJ KOD
-  pass
+    if not pesel.isnumeric() or pesel.__len__() != 11:
+        return False
+    else:
+        wights = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
+        correct_last_digit = 0
+        for i in range(len(pesel) - 1):
+            correct_last_digit += (int(pesel[i]) * wights[i])
+        correct_last_digit = 10 - correct_last_digit % 10
+        return correct_last_digit == int(pesel[-1])
+
 
 print(validate_pesel('12345678901'))  # False
 print(validate_pesel('44051401458'))  # True
