36ca978054563db3b65ab6317ff9ecea4a8e0a91
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-21 21:57:24 +0200

    C05 ex 1 - nothong special

--- Kolokwium1_PPY/C05_Classes/Z1 (1).py
+++ Kolokwium1_PPY/C05_Classes/Z1 (1).py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
-'''
-- Napisz klasę Kwadrat, Trójkąt, które są subklasami klasy Wielokąt
-- Klasa Wielokąt ma zawierać  
-    -Metodę askNSides(), która pyta użytkownika o ilość boków wielokąta (która się przyda tylko, gdy wywołamy obiekt poprzez klasę Wielokąt)
+"""
+- Napisz klasę Kwadrat, Trójkąt, które są sub klasami klasy Wielokąt
+- Klasa Wielokąt ma zawierać
+    -Metodę askNSides(), która pyta użytkownika o ilość boków wielokąta (która się przyda tylko, gdy wywołamy obiekt
+    poprzez klasę Wielokąt)
     -Metode inputSides(), która przyjmuje (od użytkownika przez input) wielkość każdego boku
     -Metode dispSides(), która wyświetla długości boków
     -Metode circuit(), która wyświetla obwód
-    
+
 - Klasa Trójkąt ma zwierać:
     -Zainicjowanie liczby boków trójkąta do 3 przez wywołanie metody __init__ klasy Polygon
     -Metode area(), która liczy pole, o wskazanych przez użytkownika długościach
-    -Metoda isright(), która sprawdza czy trójkąt jest prostkątny
-    
+    -Metoda isRight(), która sprawdza, czy trójkąt jest prostokątny
+
 - Klasa Kwadrat ma zwierać:
     -Zainicjowanie liczby boków kwadratu do odpowiednio 1 przez wywołanie metody __init__ klasy Polygon
     -Metode area(), która liczy pole, o wskazanych przez użytkownika długościach
-'''
+"""
