1686ab19073aaded95347cb176a41371af06f7f2
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-21 21:06:34 +0200

    add files from C04 - Files and Exceptions

---
+++ Kolokwium1_PPY/C04_Files_and_Exceptions/Z1 (1).py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+'''
+1. Utwórz katalog o nazwie zajecia
+2. Do pliku dzis.txt zapisz dzisiejszą datę
+3. Przenieść plik dzis.txt do katalogu zajecia
+4. Utworz listę integerow z w zakresie 1-15.
+5. Do pliku liczby.txt w katalogu zajecia zapisz liczby z utworzonej listy, ale tak, aby każdy był w nowej linijce. Upewnij się, że dane nie zostały nadpisane.
+6. Do pliku liczby_parzyste.txt zapisz liczby parzyste z utworzonej wcześniej listy, a do pliku liczby_nieparzyste.txt zapisz liczby nieparzyste z tej listy.
+7. Wszystkie te pliki z polecenia 6 umieść w katalogu zajecia albo przy tworzeniu albo po zapisaniu do nich.
+8. Wyświetl zawartość katalogu zajecia
+'''
+
+#wynikiem ma być ["dzis.txt", "liczby.txt", "liczby_nieparzyste.txt", "liczby_parzyste.txt]
---
+++ Kolokwium1_PPY/C04_Files_and_Exceptions/Z2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+'''
+Napisz program, który będzie konwertować temperature między stopniami Celcjusza i stopniami Farenhita.
+Program pownineń pytać użytkownika o na co chce przekonwertować i jaką wartość
+Program ma obsługiwać błędy poprzez try i except i kończyć program przy:
+    - dzieleniu przez 0
+    - wpisaniu przy pytaniu o konwersje coś innego niż F lub C
+    - wpisaniu przy pytaniu o wartość coś innego niż int lub float
+'''
